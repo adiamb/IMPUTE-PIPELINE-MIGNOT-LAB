9d25b261a1552edfd9fc73613d814f9e1af2fc80
Aditya Ambati <ambati@stanford.edu>
2018-02-14 11:26:26 -0800

    Update CAT_IMPUTE_FILES_STEP4.sh
    tar the log files from the impute function and cat all the chr files

--- CAT_IMPUTE_FILES_STEP4.sh
+++ CAT_IMPUTE_FILES_STEP4.sh
@@ -3,6 +3,6 @@
 #$ -l h_vmem=10G
 #$ -l h_rt=12:00:00
 #$ -t 1-22
-mkdir impute_info
-mv CHR\$SGE_TASK_ID_"$1"\.\*\_\* impute_info/
-cat CHR\$SGE_TASK_ID_"$1"\.\* \> CHR\$SGE_TASK_ID_"$i".impute2
+tar -czvf --remove-files CHR$SGE_TASK_ID.tar.gz CHR$SGE_TASK_ID\_"$1".*_*
+cat CHR$SGE_TASK_ID\_"$1".* > CHR$SGE_TASK_ID\_"$1".impute2
+
