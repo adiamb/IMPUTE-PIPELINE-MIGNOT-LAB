f137d6e5a8dbf6b6ac8d01c60b3b1937bb2377f9
Aditya Ambati <ambati@stanford.edu>
2018-02-16 10:12:09 -0800

    migration to SLURM from SUN GRID ENGINE
    unfortunately sun grid engine is going away on scg4 and being replaced with SLURM which uses a totally different commands to submit jobs

--- PLINK_SPLIT_CHR_STEP1.sh
+++ PLINK_SPLIT_CHR_STEP1.sh
@@ -1,13 +1,15 @@
-#!/bin/bash
-command="plink --bfile "$1" --chr \$SGE_TASK_ID --make-bed --out "$1"_CHR\$SGE_TASK_ID"
+#!/bin/bash 
+command="plink --bfile "$1" --chr \$SLURM_ARRAY_TASK_ID --make-bed --out "$1"_CHR\$SLURM_ARRAY_TASK_ID"
 touch PLINK_SPLIT.sh
 chmod 755 PLINK_SPLIT.sh
-echo \#\!/bin/bash >PLINK_SPLIT.sh
-echo \#$ -N PLINK_SPLIT_CHR >>PLINK_SPLIT.sh
-echo \#$ -l h_vmem=10G >>PLINK_SPLIT.sh
-echo \#$ -l h_rt=12:00:00 >>PLINK_SPLIT.sh
-echo \#$ -t 1-22 >>PLINK_SPLIT.sh
+chmod 755 PLINK_SPLIT.sh
+echo \#\!/bin/bash -l >PLINK_SPLIT.sh
+echo \#SBATCH --job-name=PLINK_SPLIT_CHR >>PLINK_SPLIT.sh
+echo \#SBATCH --mem-per-cpu=4000 >>PLINK_SPLIT.sh
+echo \#SBATCH --time=01:00:00 >>PLINK_SPLIT.sh
+echo \#SBATCH --array=1-22 >>PLINK_SPLIT.sh
+echo \#SBATCH --account=mignot >>PLINK_SPLIT.sh
 echo "module load plink/1.90" >>PLINK_SPLIT.sh
 echo $command >> PLINK_SPLIT.sh
-qsub -V -cwd PLINK_SPLIT.sh
+sbatch --export=ALL PLINK_SPLIT.sh
 
