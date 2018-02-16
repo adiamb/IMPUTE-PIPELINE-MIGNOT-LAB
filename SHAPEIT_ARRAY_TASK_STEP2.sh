281c0c6b90d335c440247de8f509fd6b02f593f7
Aditya Ambati <ambati@stanford.edu>
2018-02-16 12:34:51 -0800

    MIgrated to SLURM shapeit
    will hold merging to main until later

--- SHAPEIT_ARRAY_TASK_STEP2.sh
+++ SHAPEIT_ARRAY_TASK_STEP2.sh
@@ -5,12 +5,12 @@ command="./shapeit --input-bed "$1"_CHR\$SLURM_ARRAY_TASK_ID.bed "$1"_CHR\$SLURM
 -T 8"
 touch shapeit_array.sh
 chmod 755 shapeit_array.sh
-echo \#\!/bin/bash >shapeit_array.sh
+echo \#\!/bin/bash -l >shapeit_array.sh
 echo \#SBATCH --job-name=shapeit_array >>shapeit_array.sh
 echo \#SBATCH --mem-per-cpu=10000 >>shapeit_array.sh
 echo \#SBATCH --time=12:00:00 >>shapeit_array.sh
 echo \#SBATCH --array=1-22 >>shapeit_array.sh
-echo \#$ -hold_jid PLINK_SPLIT_CHR >>shapeit_array.sh
-echo \#$ -pe shm 4 >>shapeit_array.sh
+echo \#SBATCH --depend=afterok:"$2"_1 >>shapeit_array.sh
+echo \#SBATCH --cpus-per-task=4 >>shapeit_array.sh
+echo \#SBATCH --account=mignot >>shapeit_array.sh
 echo $command >>shapeit_array.sh
-qsub -V -cwd shapeit_array.sh
