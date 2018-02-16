b8038da0a6444194917f478f60b4b9a309e4ae6b
Aditya Ambati <ambati@stanford.edu>
2018-02-16 12:39:38 -0800

    SLURM Migration in process
    will hold commit to master until SGE is depreciated

--- IMPUTE_PIPELINE_V1_AA_FEB2018.sh
+++ IMPUTE_PIPELINE_V1_AA_FEB2018.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-echo "###################################################################################################################" 
-echo "                            IMPUTE PIPELINE VERSION 1 SCG4  Version date Feb 1 2018" 
-echo "                                     SCG4 utilizing parallel computation"
+echo "################################################################################################################" 
+echo "                            IMPUTE PIPELINE VERSION 1 SCG4  Version date Feb 16 2018 SLURM VERSION" 
+echo "                                     SCG4 utilizing parallel computation SLURM VERSION"
 echo "                                              Aditya Ambati"
 echo "                                           ambati@stanford.edu"
-echo "###################################################################################################################"
+echo "################################################################################################################"
 
 echo " Plink is splitting chromosomes - only autosomomes "
-./PLINK_SPLIT_CHR_STEP1.sh $1
+./PLINK_SPLIT_SLURM.sh $1
+JOBID0=$(sbatch --export=ALL PLINK_SPLIT.sh)
+if ! echo ${JOBID0} | grep -q "[1-9][0-9]*$"; then
+   echo "Job(s) submission failed."
+   echo ${JOBID0}
+   exit 1
+else
+   job0=$(echo ${JOBID0} | grep -oh "[1-9][0-9]*$")
+fi
 
 echo " Holding shapeit job for plink to finish splitting chromosome"
-./SHAPEIT_ARRAY_TASK_STEP2.sh $1
-
+./SHAPEIT_ARRAY_TASK_SLURM.sh $1 $job0
+JOBID1=$(sbatch --export=ALL shapeit_array.sh)
+if ! echo ${JOBID1} | grep -q "[1-9][0-9]*$"; then
+   echo "Job(s) submission failed."
+   echo ${JOBID1}
+   exit 1
+else
+   job1=$(echo ${JOBID1} | grep -oh "[1-9][0-9]*$")
+fi
 echo " Holding impute tasks until shapeit jobs completes, each chromosome is imputed in 1mb chunks "
-python IMPUTE_HEADCOMMAND_STEP3.py $1 ## this should trigger "IMPUTE_LOOP_PHASE1_STEP3.sh"
-
-echo " Imputation in progress "
+./IMPUTE_LOOP_SLURM.sh 1 5 $1 $job1 ## python script will go here to submit jobs
