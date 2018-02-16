81748d1a1c67cacb97ca012f29ce90c2146ed289
Aditya Ambati <ambati@stanford.edu>
2018-02-16 10:26:23 -0800

    Migration to SLURM work in progress
    I will not commit to main code until all bugs have been addressed, so we can continue to use the SGE submission

--- SHAPEIT_ARRAY_TASK_STEP2.sh
+++ SHAPEIT_ARRAY_TASK_STEP2.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-command="./shapeit --input-bed "$1"_CHR\$SGE_TASK_ID.bed "$1"_CHR\$SGE_TASK_ID.bim "$1"_CHR\$SGE_TASK_ID.fam \
--M /srv/gsfs0/projects/mignot/IMPUTE_REFERENCE_PHASE1/genetic_map_chr\$SGE_TASK_ID\_combined_b37.txt \
--O "$1"_CHR\$SGE_TASK_ID \
--T 4"
+command="./shapeit --input-bed "$1"_CHR\$SLURM_ARRAY_TASK_ID.bed "$1"_CHR\$SLURM_ARRAY_TASK_ID.bim "$1"_CHR\$SLURM_ARRAY_TASK_ID.fam \
+-M /srv/gsfs0/projects/mignot/IMPUTE_REFERENCE_PHASE1/genetic_map_chr\$SLURM_ARRAY_TASK_ID\_combined_b37.txt \
+-O "$1"_CHR\$SLURM_ARRAY_TASK_ID \
+-T 8"
 touch shapeit_array.sh
 chmod 755 shapeit_array.sh
 echo \#\!/bin/bash >shapeit_array.sh
-echo \#$ -N shapeit_array >>shapeit_array.sh
-echo \#$ -l h_vmem=10G >>shapeit_array.sh
-echo \#$ -l h_rt=12:00:00 >>shapeit_array.sh
-echo \#$ -t 1-22 >>shapeit_array.sh
+echo \#SBATCH --job-name=shapeit_array >>shapeit_array.sh
+echo \#SBATCH --mem-per-cpu=10000 >>shapeit_array.sh
+echo \#SBATCH --time=12:00:00 >>shapeit_array.sh
+echo \#SBATCH --array=1-22 >>shapeit_array.sh
 echo \#$ -hold_jid PLINK_SPLIT_CHR >>shapeit_array.sh
 echo \#$ -pe shm 4 >>shapeit_array.sh
 echo $command >>shapeit_array.sh
